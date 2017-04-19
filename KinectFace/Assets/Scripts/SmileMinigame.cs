caacb23f4fad43e307438cb03c2117cb50bcea11
Matthew Harris <m.c.harris@hotmail.co.uk>
2017-04-19 15:32:16 +0100

    flower now grows on the smile minigame

--- KinectFace/Assets/Scripts/SmileMinigame.cs
+++ KinectFace/Assets/Scripts/SmileMinigame.cs
@@ -8,11 +8,15 @@ using UnityEngine;
 /// </summary>
 public class SmileMinigame : ExerciseManager
 {
+    public GameObject flower;
+    private GameObject actualflower;
+    private int smileCount;
 	override protected void Start()
     {
         base.Start();
         // The current exercise needs to be loaded from somewhere, we need to have the poses saved in some file
         _currentExercise = PoseFileHandler.LoadExercise("smile");
+        actualflower = Instantiate(flower, new Vector3(0, -8), Quaternion.identity);
 	}
 	
 	void Update()
@@ -26,8 +30,21 @@ public class SmileMinigame : ExerciseManager
             Pose currentPose = _kinect.GetCurrentPose();
             if (_currentExercise.IsSimilar(currentPose, _tolerance))
             {
-                Debug.Log("You've finished the smile exercise! To the next one! -> ");
-                GameManager.instance.NextMiniGame();
+                smileCount++;
+                if (smileCount > 100)
+                {
+                    Debug.Log("You have completed the smile exercise! onto the next one! -> ");
+                    GameManager.instance.NextMiniGame();
+                }
+                else
+                {
+                    Debug.Log(smileCount);
+                    float flowerGrow = ((float)smileCount / 100) * 90;
+                    Debug.Log(flowerGrow);
+                    actualflower.transform.Translate(new Vector3(0, 0.1f));
+                }
+                //Debug.Log("You've finished the smile exercise! To the next one! -> ");
+                //GameManager.instance.NextMiniGame();
             }
         }
 	}
